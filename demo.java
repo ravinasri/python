3d334a6787d385aa33fee2095b43155d2cd7d3f5
ravinasri <41889882+ravinasri@users.noreply.github.com>
2018-08-06 12:05:33 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,19 @@
+public class demo
+{
+public static void main(String args[])
+{
+int number;
+if(number>0)
+{
+System.out.println(number+"is a positive number");
+}
+else if(number<0)
+{
+System.out.println(number+"is a negative number");
+}
+else
+{
+System.out.println(number+"is neither positive nor negative");
+}
+}
+}
