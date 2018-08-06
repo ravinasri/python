5c39673cee1dedaef80dd9e4c15fbdacf2ae0dd3
ravinasri <41889882+ravinasri@users.noreply.github.com>
2018-08-06 12:12:12 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -3,6 +3,7 @@ public class demo
 public static void main(String args[])
 {
 int number;
+  System.out.println("enter the integer number");
 if(number>0)
 {
 System.out.println(number+"is a positive number");
