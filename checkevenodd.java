38ae71cf6e5ac0fa04f92a6951bfdb3156eb42e1
ravinasri <41889882+ravinasri@users.noreply.github.com>
2018-08-06 12:19:48 +0530

    Create checkevenodd.java

---
+++ checkevenodd.java
@@ -0,0 +1,15 @@
+import java.util.scanner;
+class checkevenodd
+{
+public static void main(String args[])
+{
+int num;
+System.out.println("enter an integer number");
+Scanner input=new scanner(System.in);
+num=input.nextInt();
+if(num%2==0)
+System.out.println("entered number is even");
+else
+System.out.println("entered number is odd");
+}
+}
